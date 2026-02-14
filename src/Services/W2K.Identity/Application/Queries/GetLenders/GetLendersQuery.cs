6d025727ee4a987525abfc86fa9739d440c6f699
Bilal Sofi <sofibilal193@gmail.com>
2026-02-14 21:58:40 +0530

    refactor: Change IFilesApiClient registration to scoped and remove GetLenders query-related files

--- src/Services/W2K.Identity/Application/Queries/GetLenders/GetLendersQuery.cs
+++
@@ -1,7 +0,0 @@
-using W2K.Common.Application;
-
-#pragma warning disable IDE0130 // Namespace does not match folder structure
-namespace W2K.Identity.Application;
-#pragma warning restore IDE0130 // Namespace does not match folder structure
-
-public readonly record struct GetLendersQuery() : IRequest<List<LenderDto>>;
